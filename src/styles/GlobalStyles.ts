06a2072da92880223d44480f93caab8337e82298
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-09-25 17:18:17 -0300

    estilos globais iniciais

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,4 +1,25 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    font-family: Arial, sans-serif;
+    font-size: 62.5%;
+  }
+
+  body{
+    width: 100%;
+    height: 100vh;
+    background-color: blue;
+  }
+
+  h1{
+    color: #FFFFFF;
+  }
 `;
